60bc0238829f7db353ca03b5ba60580086576d5a
ins099 <alaminsaram92@gmail.com>
2021-06-09 14:42:45 +0500

    Sign out Saga init

--- src/constants/Screens.js
+++ src/constants/Screens.js
@@ -3,11 +3,8 @@ export const LANGUAGE_SCREEN = 'Language';
 export const SIGNUP_SCREEN = 'SignUp';
 export const SIGNIN_SCREEN = 'SignIn';
 export const FORGOT_PASSWORD_SCREEN = 'ForgotPassword';
-<<<<<<< HEAD
 export const AD_SCREEN = 'AdScreen';
 export const FORM_SCREEN = 'FORM_SCREEN';
-=======
 export const IMAGE_PICKER_SCREEN = 'ImagePickerScreen';
 // export const AD_SCREEN = 'AdScreen';
 // export const AD_SCREEN = 'AdScreen';
->>>>>>> e4fb59e774234e3679dec5495f14a816f582e6a5
---
+++ src/redux/saga/SignOutSaga.js
@@ -0,0 +1,44 @@
+import {HOME, SIGNIN_SCREEN} from '../../constants/Screens';
+import {getItem, setItem} from 'helpers/Localstorage';
+import {Alert} from 'react-native';
+import {put} from 'redux-saga/effects';
+
+import {RestClient} from '../../network/RestClient';
+import {SIGN_OUT_SUCCESS, SIGN_OUT_FAILURE} from '../../redux/actionTypes';
+import * as NavigationService from '../../../NavigationService';
+import {
+  FETCH_ADDRESS_SUCCESS,
+  FETCH_ORDER_SUCCESS,
+  FETCH_USER_CART_SUCCESS,
+  FETCH_USER_FAVOURITE_SUCCESS,
+} from '../actionTypes';
+export function* signoutSaga() {
+  try {
+    // let userProfile = yield getItem('@userProfile');
+    // userProfile = JSON.parse(userProfile);
+    // let signedOutUserProfile = {
+    //   language: userProfile.language,
+    //   currency: userProfile.currency,
+    //   notification: true,
+    // };
+    // console.log(signedOutUserProfile, 'userProfile');
+
+    // yield setItem('@userProfile', JSON.stringify(signedOutUserProfile));
+    RestClient.setHeader('Authorization', null);
+    // yield put({type: SIGN_OUT_SUCCESS, payload: });
+    // yield put({type: FETCH_ORDER_SUCCESS, payload: []});
+    // yield put({
+    //   type: FETCH_USER_FAVOURITE_SUCCESS,
+    //   payload: null,
+    // });
+    // yield put({type: FETCH_USER_CART_SUCCESS, payload: null});
+    // yield put({type: FETCH_ADDRESS_SUCCESS, payload: {data: []}});
+    NavigationService.navigate('Auth', {
+      screen: SIGNIN_SCREEN,
+      params: {cleanBack: true},
+    });
+  } catch (error) {
+    yield put({type: SIGN_OUT_FAILURE, error});
+    console.log(error);
+  }
+}
